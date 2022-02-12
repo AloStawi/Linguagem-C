987c02dafcc6308b81fefc9e0c123df916189be7
AloStawi <isabelle.nascimento@aluno.ifsp.edu.br>
2022-02-11 22:32:13 -0300

    Add files via upload

---
+++ Lista 2/Casa/1.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+
+int main(){
+int n;
+printf ("Insira seu numero: ");
+scanf ("%d", &n);
+if (n%2==0)
+printf ("Seu numero eh par");
+else 
+printf ("Seu numero eh impar");
+
+getch();
+return 0;
+}
